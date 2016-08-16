cd567e2d60210eecd78870855f1b3a8c8228490f
ahm lee <ahmsolo@gmail.com>
2016-08-15 19:50:35 -0700

    Started calculating distances between lats/lons

--- api_test.py
+++ api_test.py
@@ -6,10 +6,6 @@ import os
 # token = os.environ['NOAA_API_TOKEN']
 token = "izzTBSCJqNwpKXnGuotVSePytUlvIUXY"
 
-stations = [9410170, 9410230, 9410660, 9410840, 9411340, 9411406, 9412110,
-          9413450, 9414290, 9414750, 9414523, 9414575, 9414863, 9415102,
-          9415144, 9414958, 9415020, 9416841, 9418767, 9419750]
-
 
 def retrieve_tides_json(stations):
     """ Retrieve high and low tides for specific station within date range """
@@ -19,7 +15,7 @@ def retrieve_tides_json(stations):
 # &end_date=20170802%2017:00&station=9415020&product=predictions&datum=navd
 # &units=metric&interval=h&time_zone=gmt&format=json
     for station in stations:
-        params = {"station": 9410170,
+        params = {"station": station,
                   # "begin_date": "20160731 00:00",
                   # "end_date": "20160802 00:00",
                   "date": "today",
@@ -40,30 +36,32 @@ def retrieve_tides_json(stations):
 def retrieve_tides_soap():
     """ Retrieve high and low tides for specific station within date range """
 
+    stations = [9410170, 9410230, 9410580, 9410680, 9410660, 9410840, 9411340, 9411399, 9411406, 9412110, 9412802, 9413450, 9413663, 9414131, 9414290, 9414317, 9414764, 9414750, 9414746, 9414358, 9414688, 9414458, 9414523, 9414509, 9414575, 9414863, 9415218, 9415143, 9415102, 9415265, 9415144, 9414811, 9415112, 9415064, 9415316, 9415056, 9415338, 9414958, 9415020, 9416409, 9416841, 9417426, 9418767, 9418723, 9418817, 9419750, 9419945]
+
     url = "http://opendap.co-ops.nos.noaa.gov/axis/webservices/highlowtidepred/response.jsp"
 # http://opendap.co-ops.nos.noaa.gov/axis/webservices/highlowtidepred/
 # response.jsp?stationId=9414958&beginDate=20160810&endDate=20160812
 # &datum=MLLW&unit=0&timeZone=0&format=xml&Submit=Submit
-    params = {"stationId": 9414958,
-              "beginDate": "20160801",
-              "endDate": "20160803",
-              "timeZone": "1",
-              "unit": "0",
-              "format": "text",
-              "datum": "MLLW",
-              "Submit": "Submit"
-              }
+    for station in stations:
+        params = {"stationId": station,
+                  "beginDate": "20160801",
+                  "endDate": "20160803",
+                  "timeZone": "1",
+                  "unit": "0",
+                  "format": "text",
+                  "datum": "MLLW",
+                  "Submit": "Submit"
+                  }
 
-    res = requests.get(url, params=params)
-    data = res.content.split('<pre>')[-1]
+        res = requests.get(url, params=params)
+        data = res.content.split('<pre>')[-1]
+        data = data.replace('</pre>', '')
 
-    f = open('9414958', 'w')
+        f = open(str(station), 'w')
 
-    contents = data.replace('</pre>', '')
+        f.write(data)
 
-    f.write(contents)
-
-    f.close()
+        f.close()
 
 def check_api():
 
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 import json
 import os
+from geopy.distance import distance
 
 app = Flask(__name__)
 
@@ -30,6 +31,35 @@ def calculate_distance():
     """ Display distance between origin and destinations """
 
     return render_template("google_maps_distance_test.html")
+
+
+@app.route("/test", methods=['POST'])
+def test():
+   
+    lat = request.form.get("lat")
+    lon = request.form.get("lon")
+
+    station1 = {"id": "9414958", "name": "Bolinas, Bolinas Lagoon", "lat": 37.9080, "lon": -122.6785}
+    station2 = {"id": "9414958", "name": "Bolinas, Bolinas Lagoon", "lat": 37.715593, "lon": -122.307468}
+
+    stations_locations = [station1, station2]
+    closest_station = None
+    closest_dist = 10000000
+    station_distances = []
+
+    for station in stations_locations:
+        slat = station["lat"]
+        slon = station["lon"]
+        dist = distance((lat, lon), (slat, slon)).miles
+
+        station_distances.append({"station": station, "dist": dist})
+
+    nearest_stations = sorted(station_distances, key=lambda d: d["dist"])[:10]
+
+    print nearest_stations
+
+    result = {"nearest_stations": nearest_stations}
+    return json.dumps(result)
 
 
 if __name__ == "__main__":
