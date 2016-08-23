c851b76062b65cffb7dbc9624fa8a2d393ac5c09
ahm lee <ahmsolo@gmail.com>
2016-08-23 15:26:10 -0700

    Nearest stations AJAX request complete

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 import json
 import os
 from geopy.distance import distance
 
+from model import connect_to_db
+from model import Station, TideDay, TideDetail
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 key = os.environ['GOOGLE_MAPS_API_KEY']
 
@@ -26,47 +30,35 @@ def display_map():
     return render_template("google_maps_api_search2.html", data=stations_locations_str, key=key)
 
 
-@app.route("/distance", methods=['POST'])
+@app.route("/", methods=['POST'])
 def calculate_distance():
     """ Display distance between origin and destinations """
 
     lat = request.form.get("lat")
     lon = request.form.get("lon")
 
-    station1 = {"id": "9414958", "name": "Bolinas, Bolinas Lagoon", "lat": 37.9080, "lon": -122.6785}
-    station2 = {"id": "9410170", "name": "San Diego", "lat": 32.7142, "lon": -117.1736}
-    station3 = {"id": "9410230", "name": "La Jolla", "lat": 32.8667, "lon": -117.258}
-    station4 = {"id": "9410580", "name": "Newport Bay Entrance", "lat": 33.6033, "lon": -117.883}
-    station5 = {"id": "9410680", "name": "Long Beach", "lat": 33.7517, "lon": -118.227}
-    station6 = {"id": "9410660", "name": "Los Angeles", "lat": 33.72, "lon": -118.272}
-    station7 = {"id": "9410840", "name": "Santa Monica", "lat": 34.0083, "lon": -118.5}
-    station8 = {"id": "9411340", "name": "Santa Barbara", "lat": 34.4083, "lon": -119.685}
-    station9 = {"id": "9411399", "name": "Gaviota State Park", "lat": 34.4694, "lon": -120.2283}
-    station10 = {"id": "9412110", "name": "Port San Luis", "lat": 35.1767, "lon": -120.76}
-
-    stations_locations = [station1, station2, station3, station4, station5, station6, station7, station8, station9, station10]
+    all_stations = Station.query.all()
 
     station_distances = []
 
-    for station in stations_locations:
-        slat = station["lat"]
-        slon = station["lon"]
+    for station in all_stations:
+        slat = station.latitude
+        slon = station.longitude
         dist = distance((lat, lon), (slat, slon)).miles
 
-        station_distances.append({"station": station, "dist": dist})
+        station_distances.append({"station": station.name, "latitude": station.latitude, "longitude": station.longitude, "dist": dist})
 
     nearest_stations = sorted(station_distances, key=lambda d: d["dist"])[:7]
 
-    print nearest_stations
-
     result = {"nearest_stations": nearest_stations}
-    return json.dumps(result)
+    return jsonify(result)
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
     app.debug = True
+    connect_to_db(app)
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
